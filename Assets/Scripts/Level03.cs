ae5df5c2132ec99a9a48c21d8034a008fc27631d
LoicBoissy <loic.boissy@icloud.com>
2023-11-28 19:26:48 -0500

    Ajout scene sabrina

---
+++ Assets/Scripts/Level03.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level03 : MonoBehaviour
+{
+     private LevelManager _levelManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+       _levelManager = LevelManager.Instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other){
+        _levelManager.LoadAsyncScene(LevelManager.Scene.SabrinaMScene);
+    }
+}
